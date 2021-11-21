2d69fb6ff9a28d2d34fab987d79699edf2b99520
JohannesHupfeld <j.hupfeld@gmail.com>
2021-11-20 21:08:14 -0500

    updated fetch url

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,10 @@
+const baseUrl = "http://localhost:3000"
+
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:3000/recipes')
-  .then(resp => resp.json())
-  .then(recipes => {
-    console.log(recipes)
+  fetch(`${baseUrl}/api/v1/recipes`) //returns a promise
+  .then(resp => resp.json()) //pareses that to json
+  .then(recipes => {   // returns an array of recipes
+    console.log(recipes);
   })
+  .catch(err => alert(err))
 })
