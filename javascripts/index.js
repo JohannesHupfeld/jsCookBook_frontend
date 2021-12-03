e55eb12afe8be81db687f06fd317244e7fcc30d1
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-03 18:03:48 -0500

    added show recipe button + converted to arrow functions + moved fetch recipes to class

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,52 +1,49 @@
-const recipesUrl = "http://localhost:3000/api/v1/recipes"
-
+const recipesUrl = "http://localhost:3000/api/v1/recipes" // global 
+const buttonShow = () => document.getElementById("button-show")
+const recipeContainer = () => document.getElementById("recipe-container")
+ 
 document.addEventListener('DOMContentLoaded', () => {
-  fetchRecipes() 
+  // Recipe.fetchRecipes() 
+
+  buttonShow().addEventListener("click", handleClick)
 
   const createRecipeForm = document.querySelector("#create-recipe-form")
-
   createRecipeForm.addEventListener("submit", (e) => handleRecipeSubmit(e))
 })
 
-// get function
-function fetchRecipes() {
-  fetch(recipesUrl) // returns a promise
-  .then(resp => resp.json()) // parses that promise to json
-  .then(recipes => { // returns an array of recipes
-    recipes.data.forEach(recipe => { // itterates over the response and show the data in a div
-      
-      let newRecipe = new Recipe(recipe, recipe.attributes) // recipe.attributes is nested under recipe, a top level object. Correspnds to recipe class constructor
-      
-      document.querySelector('#recipe-container').innerHTML += newRecipe.renderRecipe()
-      
-    })
-  })
-  .catch(err => alert(err))
+const handleClick = () => {
+  if (recipeContainer().children.length < 1) {
+    Recipe.fetchRecipes()
+  } else {
+    recipeContainer().innerHTML = ""
+  }
 }
 
-function handleRecipeSubmit(e) {
-  e.preventDefault()
+const handleRecipeSubmit = (e) => { // callback to submit form createRecipeForm. grabs all the values for my inputs
+  e.preventDefault()  
   const nameInput = document.querySelector('#input-name').value 
   const ingredientsInput = document.querySelector('#input-ingredients').value
   const instructionsInput = document.querySelector('#input-instructions').value 
   const urlInput = document.querySelector('#input-url').value
   const categoryId = parseInt(document.querySelector('#categories').value) // parseInt turns the string into an integer
-  postRecipe(nameInput, ingredientsInput, instructionsInput, urlInput, categoryId)
+  postRecipe(nameInput, ingredientsInput, instructionsInput, urlInput, categoryId) //invokes postRecipe and passes through the values
 }
 
-function postRecipe(name, ingredients, instructions, image_url, category_id) {
-  const bodyData = {name, ingredients, instructions, image_url, category_id}
-  fetch(recipesUrl, {
+// post function
+const postRecipe = (name, ingredients, instructions, image_url, category_id) => { // posts recipes 
+  // building body object outside of fetch
+  const bodyData = {name, ingredients, instructions, image_url, category_id} // since key and value have the same name theres no need to repeat them
+  fetch(recipesUrl, { // returns a promise
     method: "POST",
     headers: {"Content-Type": 'application/json'},
-    body: JSON.stringify(bodyData)
+    body: JSON.stringify(bodyData) //converts the object in a JSON string
   })
-  .then(resp => resp.json())
-  .then(recipe => {
-    const recipeData = recipe.data 
-    // render json response
+  .then(resp => resp.json()) // parses that promise into json
+  .then(recipe => { // returns an array of json ---recipe is json
+    const recipeData = recipe.data // data points to a single object of attributes 
+    // render json response       ID             attributes 
     let newRecipe = new Recipe(recipeData, recipeData.attributes) 
-      
+    // creates a new instance of my recipe class
     document.querySelector('#recipe-container').innerHTML += newRecipe.renderRecipe()
   })
   .catch(err => alert(err))
